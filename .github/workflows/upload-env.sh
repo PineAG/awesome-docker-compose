d4dd376f8a12f1f1abff98cd1fce7aff414d146f
PineAG <pein.agail@gmail.com>
2021-06-29 22:24:35 +0000

    use gh token

--- .github/workflows/upload-env.sh
+++ .github/workflows/upload-env.sh
@@ -1,15 +1,14 @@
 #/usr/bin/env bash
 
 upload-env() {
-    remote_url=$(git remote get-url origin)
     echo "/$1" >> .gitignore
     cd $1
     git init --initial-branch=env-$1
     git add .
-    git config user.name PineAG
-    git config user.email pein.agail@gmail.com
+    git config user.name ${{ secrets.GH_USER }}
+    git config user.email "${{ secrets.GH_MAIL }}"
     git commit -m "Init docker-compose.yml for $1" --allow-empty
-    git remote add origin $remote_url
+    git remote add origin "https://${{ secrets.GH_TOKEN}}@github.com/PineAG/awesome-docker-compose.git"
     git push origin env-$1
     cd ..
 }
