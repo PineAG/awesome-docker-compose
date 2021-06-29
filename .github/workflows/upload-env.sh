1e6b7f188a24f3952feb21f6644f09242f2a27ca
PineAG <pein.agail@gmail.com>
2021-06-29 22:26:00 +0000

    fix passing token

--- .github/workflows/upload-env.sh
+++ .github/workflows/upload-env.sh
@@ -1,14 +1,15 @@
 #/usr/bin/env bash
+gh_token=$1
 
 upload-env() {
     echo "/$1" >> .gitignore
     cd $1
     git init --initial-branch=env-$1
     git add .
-    git config user.name ${{ secrets.GH_USER }}
-    git config user.email "${{ secrets.GH_MAIL }}"
+    git config user.name PineAG
+    git config user.email pein.agail@gmail.com
     git commit -m "Init docker-compose.yml for $1" --allow-empty
-    git remote add origin "https://${{ secrets.GH_TOKEN}}@github.com/PineAG/awesome-docker-compose.git"
+    git remote add origin "https://$gh_token@github.com/PineAG/awesome-docker-compose.git"
     git push origin env-$1
     cd ..
 }
